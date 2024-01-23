a0b716c19f78e0dce67b9f75f350e21b4384af7f
hyun <kingmandoo95@gmail.com>
2024-01-23 18:15:31 +0900

    test : healthcheck string -> html

--- src/main/java/com/challnege/delivery/global/controller/AlbHealthcheck.java
+++ src/main/java/com/challnege/delivery/global/controller/AlbHealthcheck.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AlbHealthcheck {
 
     @GetMapping("/healthcheck")
-    public ResponseEntity<String> healthcheck() {
-        return new ResponseEntity<>(HttpStatus.OK);
+    public String healthcheck() {
+        return "OK";
     }
 
     @GetMapping("/health-check")
